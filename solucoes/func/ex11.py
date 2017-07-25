f5beb1cd95d0633e2d870706d416e2861f3e5f92
Williamkleinds <williamfelipeklein@gmail.com>
2017-07-24 22:00:21 -0300

    Proposto por gafanhotos

---
+++ solucoes/func/ex11.py
@@ -0,0 +1,16 @@
+#Proposto por @Yasmim#7532
+
+Q1 - string.lower()
+Q2 - string = len(string) * '?'
+Q3 - string.count('char')
+Q4 - char in string
+Q5 -
+lista = []
+for i in range(len(string)):
+    if string[i] == char:
+        lista.append(i)
+Q6 -
+for i in lista:
+    string = string.replace(string[i], char, 1)
+Q7 -
+char in lista
